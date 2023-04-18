be1280a40b171f79d24d753a3e05a6d35ec12efd
Louisvranderick <73151698+Louisvranderick@users.noreply.github.com>
2023-04-18 17:01:04 -0400

    Add files via upload

---
+++ KevinF., LouisV. & ShayanH_TP3 4/assets/scripts/script.js
@@ -0,0 +1,10 @@
+function ShopNum() { /* fonction pour aller cherche le chiffre dans count et ajouter 1 quand on ajoute quelque chose au panier */  
+     document.getElementById(numVal).innerHTML = document.getElementsById(ItemNum).value 
+} 
+
+
+
+function changeCount() { 
+
+    
+}
